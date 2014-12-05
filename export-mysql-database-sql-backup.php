2ae187898f61c87e9a574a0703d59596dd0ee708
Tazo Todua <tazotodua@gmail.com>
2014-12-05 14:14:52 +0400

    Update export-mysql-database-sql-backup.php

--- export-mysql-database-sql-backup.php
+++ export-mysql-database-sql-backup.php
@@ -4,16 +4,17 @@ EXPORT_TABLES("localhost","user","pass","db_name");  //to IMPORT, see the import
 <?php
 function EXPORT_TABLES($host,$user,$pass,$name,  $tables=false, $backup_name=false )
 {
-	$link = mysqli_connect($host,$user,$pass,$name); if (mysqli_connect_errno()){ echo "ConnecttError: " . mysqli_connect_error();} mysqli_select_db($link,$name);	mysqli_query($link,"SET NAMES 'utf8'");
-	$queryTables = mysqli_query($link,'SHOW TABLES'); while($row = mysqli_fetch_row($queryTables)) { $target_tables[] = $row[0]; }	if($tables !== false) { $target_tables = array_intersect( $target_tables, explode(',',$tables)); }
+	$mysqli = new mysqli($host,$user,$pass,$name); if ($mysqli->connect_errno){ echo "ConnecttError: " . $mysqli->connect_error;} $mysqli->select_db($name); $mysqli->query("SET NAMES 'utf8'");
+	$queryTables = $mysqli->query('SHOW TABLES'); while($row = $queryTables->fetch_row()) { $target_tables[] = $row[0]; }	if($tables !== false) { $target_tables = array_intersect( $target_tables, explode(',',$tables)); }
 	
 	$content='';    //start cycle
 	foreach($target_tables as $table){
-		$result	= mysqli_query($link,'SELECT * FROM '.$table); $fields_amount = mysqli_num_fields($result);$rows_num=mysqli_num_rows($result); $row2= mysqli_fetch_row(mysqli_query($link, 'SHOW CREATE TABLE '.$table)); 
-		$content	.= "\n\n".$row2[1].";\n\n";
+		$result	= $mysqli->query('SELECT * FROM '.$table); 	$fields_amount=$result->field_count;  $rows_num=$mysqli->affected_rows;
+		$res = $mysqli->query('SHOW CREATE TABLE '.$table);	$TableMLine=$res->fetch_row();
+		$content	.= "\n\n".$TableMLine[1].";\n\n";
 		for ($i = 0; $i < $fields_amount; $i++) {
 			$st_counter= 0;
-			while($row = mysqli_fetch_row($result))	{
+			while($row = $result->fetch_row())	{
 					//when started (and every after 100 command cycle)
 					if ($st_counter%100 == 0 || $st_counter == 0 )	{$content .= "\nINSERT INTO ".$table." VALUES";}
 				$content .= "\n(";
@@ -31,6 +32,6 @@ function EXPORT_TABLES($host,$user,$pass,$name,  $tables=false, $backup_name=fal
 
 	//save file
 	$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
-	header('Content-Type: application/octet-stream');	header("Content-Transfer-Encoding: Binary"); header("Content-disposition: attachment; filename=\"".$backup_name."\""); echo $content; exit;
+	header('Content-Type: application/octet-stream');	header("Content-Transfer-Encoding: Binary"); header("Content-disposition: attachment; filename=\"".$backup_name."\"");  echo $content; exit;
 }
 ?>

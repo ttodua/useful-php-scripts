83ec292956933e0772b2a06ac22a59e26411af4e
Tazo Todua <tazotodua@gmail.com>
2015-04-25 02:36:47 +0400

    Update get-remote-url-content-data.php

--- get-remote-url-content-data.php
+++ get-remote-url-content-data.php
@@ -1,42 +1,67 @@
+<?php
 ====================================== USAGE=============================
-echo get_remote_data('http://example.com', true, "var2=something&var3=blabla" ); 
-											// FOLLOWLOCATION enabled; POST REQUEST; (p.s. if you dont want POST request, then remove the third argument from function and then it wont be POST request anymore.
-	
-CODE :
-	function get_remote_data($url, $use_FOLLOWLOCATION=true, $post_paramtrs=false, $from_mobile=false )	{
-		$c = curl_init();curl_setopt($c, CURLOPT_URL, $url);
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-		if ($post_paramtrs){curl_setopt($c, CURLOPT_POST, TRUE); curl_setopt($c, CURLOPT_POSTFIELDS, "var1=bla&".$post_paramtrs);}
-		curl_setopt($c, CURLOPT_SSL_VERIFYHOST,false);
-		curl_setopt($c, CURLOPT_USERAGENT, ($from_mobile) ? "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1C25 Safari/419.3" :  "Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0");
-		curl_setopt($c, CURLOPT_SSL_VERIFYPEER,false);
-		curl_setopt($c, CURLOPT_MAXREDIRS, 10);
-		curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 9);
-		curl_setopt($c, CURLOPT_TIMEOUT, 60);
-		curl_setopt($c, CURLOPT_REFERER, $url);
-		curl_setopt($c, CURLOPT_ENCODING, 'gzip,deflate');
-		curl_setopt($c, CURLOPT_AUTOREFERER, true);
-		//$header[0] = "Accept: text/xml,application/xml,application/xhtml+xml," . "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"; $header[]="Cache-Control: max-age=0"; $header[]="Connection: keep-alive"; $header[]="Keep-Alive: 300"; $header[]="Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7"; $header[] = "Accept-Language: en-us,en;q=0.5"; $header[] = "Pragma: ";		curl_setopt($c, CURLOPT_HTTPHEADER, $header); curl_setopt($c, CURLOPT_HEADER, true);	
+echo get_remote_data('http://example.com/');                                   //simple request
+echo get_remote_data('http://example.com/', "var2=something&var3=blabla" );    //POST request 										
 
-		$data = curl_exec($c);	$status=curl_getinfo($c); curl_close($c);
-								//correct Links(i.e. href="PICTURE.JPG" to href="http://site.com/images/PICTURE.JPG" )
-								preg_match('/(http:|https:)\/\/(.*\/|.*)/', $url,$n); foreach( array_merge(range('a','z'), range('A','Z'), array('.','..'), range('0','9'))  as $i) 	{
-									if ($i != 'h' && $i != 'H'){ //except h, because it might be http://..
-										$data=str_replace( array('src="'.$i,'href="'.$i,'action="'.$i), array('src="'.$n[0].$i,'href="'.$n[0].$i,'action="'.$n[0].$i), $data);
-									}
-								}
-		//if TURNED OFF "FOLLOWLOCATION"
-		if($status['http_code']==200) {return $data;}
-		elseif (!$use_FOLLOWLOCATION){
-			if($status['http_code'] == 301 || $status['http_code'] == 302) {
-				list($header) = explode("\r\n\r\n", $data, 2);	preg_match("/(Location:|URI:)[^(\n)]*/", $header, $matches);
-				$url = trim(str_replace($matches[1],"",$matches[0])); $url_parsed = parse_url($url);
-				return (isset($url_parsed))? get_remote_data($url, $use_FOLLOWLOCATION, $post_paramtrs, $from_mobile) : "ERRORCODE11:<br/>can't catch redirected url. LAST RESPONSE:<br/><br/>$data";
-			}
-			else {$f=''; foreach($status as $key=>$e){$f.='['.$key.']'.$e.' ';} return "ERRORCODE13:<br/>$f<br/>$url";}
+	(**NOTICE:**  remote urls are automatically re-corrected!  (for exampled:  src="./imageblabla.png"  --------> src="http://example.com/path/imageblabla.png" !! )
+
+	
+	
+================================================== compressed version:=========================================
+function get_remote_data($url, $post_paramtrs=false)	{
+   $c = curl_init();curl_setopt($c, CURLOPT_URL, $url);curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);	if($post_paramtrs){curl_setopt($c, CURLOPT_POST,TRUE);	curl_setopt($c, CURLOPT_POSTFIELDS, "var1=bla&".$post_paramtrs );}	curl_setopt($c, CURLOPT_SSL_VERIFYHOST,false);curl_setopt($c, CURLOPT_SSL_VERIFYPEER,false);curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0"); curl_setopt($c, CURLOPT_COOKIE, 'CookieName1=Value;');	curl_setopt($c, CURLOPT_MAXREDIRS, 10);  $follow_allowed= ( ini_get('open_basedir') || ini_get('safe_mode')) ? false:true;  if ($follow_allowed){curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);}curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 9);curl_setopt($c, CURLOPT_REFERER, $url);curl_setopt($c, CURLOPT_TIMEOUT, 60);curl_setopt($c, CURLOPT_AUTOREFERER, true);  		curl_setopt($c, CURLOPT_ENCODING, 'gzip,deflate');$data=curl_exec($c);$status=curl_getinfo($c);curl_close($c);preg_match('/(http(|s)):\/\/(.*?)\/(.*\/|)/si',  $status['url'],$link);$data=preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/|\/)).*?)(\'|\")/si','$1=$2'.$link[0].'$3$4$5', $data);$data=preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/)).*?)(\'|\")/si','$1=$2'.$link[1].'://'.$link[3].'$3$4$5', $data);if($status['http_code']==200) {return $data;} elseif($status['http_code']==301 || $status['http_code']==302) { if (!$follow_allowed){if (!empty($status['redirect_url'])){return get_remote_data($status['redirect_url'], $post_paramtrs);}else { preg_match('/href\=\"(.*?)\"/si',$data,$m); if (!empty($m[1])){ return get_remote_data($m[1], $post_paramtrs);}}}} return "ERRORCODE22 with $url!!<br/>Last status codes<b/>:".json_encode($status)."<br/><br/>Last data got<br/>:$data";
+}
+
+
+
+
+
+
+
+
+
+================================================== docummented version (100% same, just with explanations): ==========
+function get_remote_data($url, $post_paramtrs=false)	{
+	$c = curl_init();curl_setopt($c, CURLOPT_URL, $url);
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+	//if parameters were passed to this function, then transform into POST method.. (if you need GET request, then simply change the passed URL)
+	if($post_paramtrs){curl_setopt($c, CURLOPT_POST,TRUE);	curl_setopt($c, CURLOPT_POSTFIELDS, "var1=bla&".$post_paramtrs );}
+	curl_setopt($c, CURLOPT_SSL_VERIFYHOST,false);                  
+	curl_setopt($c, CURLOPT_SSL_VERIFYPEER,false);
+	curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0"); 
+	curl_setopt($c, CURLOPT_COOKIE, 'CookieName1=Value;');
+					//We'd better to use the above command, because the following command gave some weird STATUS results..
+					//$header[0]= $user_agent="User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0";  $header[]="Cookie:CookieName1=Value;"; $header[]="Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";  $header[]="Cache-Control: max-age=0"; $header[]="Connection: keep-alive"; $header[]="Keep-Alive: 300"; $header[]="Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7"; $header[] = "Accept-Language: en-us,en;q=0.5"; $header[] = "Pragma: ";  curl_setopt($c, CURLOPT_HEADER, true);     curl_setopt($c, CURLOPT_HTTPHEADER, $header);
+					
+	curl_setopt($c, CURLOPT_MAXREDIRS, 10);    
+	$follow_allowed= ( ini_get('open_basedir') || ini_get('safe_mode')) ? false:true;  if ($follow_allowed){curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);}
+	curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 9);
+	curl_setopt($c, CURLOPT_REFERER, $url);    
+	curl_setopt($c, CURLOPT_TIMEOUT, 60);
+	curl_setopt($c, CURLOPT_AUTOREFERER, true);  
+	curl_setopt($c, CURLOPT_ENCODING, 'gzip,deflate');
+	$data=curl_exec($c);$status=curl_getinfo($c);curl_close($c);
+	
+	preg_match('/(http(|s)):\/\/(.*?)\/(.*\/|)/si',  $status['url'],$link);	
+	//correct assets URLs(i.e. retrieved url is: http://site.com/DIR/SUBDIR/page.html... then href="./image.JPG" becomes href="http://site.com/DIR/SUBDIR/image.JPG", but  href="/image.JPG" needs to become href="http://site.com/image.JPG")
+	
+	//inside all links(except starting with HTTP,javascript:,HTTPS,//,/ ) insert that current DIRECTORY url (href="./image.JPG" becomes href="http://site.com/DIR/SUBDIR/image.JPG")
+	$data=preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/|\/)).*?)(\'|\")/si','$1=$2'.$link[0].'$3$4$5', $data);     
+	//inside all links(except starting with HTTP,javascript:,HTTPS,//)    insert that DOMAIN url (href="/image.JPG" becomes href="http://site.com/image.JPG")
+	$data=preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/)).*?)(\'|\")/si','$1=$2'.$link[1].'://'.$link[3].'$3$4$5', $data);    
+	if($status['http_code']==200) {return $data;} 
+	elseif($status['http_code']==301 || $status['http_code']==302) { 
+		//if we FOLLOWLOCATION was not allowed, then re-get REDIRECTED URL
+		//p.s. WE dont need "else", because if FOLLOWLOCATION was allowed, then we wouldnt have come to this place, because 301 could already auto-followed by curl  :)
+		if (!$follow_allowed){
+			//if REDIRECT URL is found in HEADER
+			if (!empty($status['redirect_url'])){ return get_remote_data($status['redirect_url'], $post_paramtrs);}
+			//if REDIRECT URL is found in OUTPUT
+			else { preg_match('/href\=\"(.*?)\"/si',$data,$m); if (!empty($m[1])){ return get_remote_data($m[1], $post_paramtrs); } }
 		}
-	}
+	} 
+	//if not met any return criteria above, then show error
+	return "ERRORCODE22 with $url!!<br/>Last status codes:".json_encode($status)."<br/><br/>Last data got:$data";
+}
 
 	
-(**NOTICE:** if you want to display images and href urls CORRECTLY, (i.e href="./imageblabla.png" to href="http://example.com/imageblabla.png" ), then this code does that too! )

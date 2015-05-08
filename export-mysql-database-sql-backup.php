845dac3ad8712f77da1575653b459d653c07f7e6
Tazo Todua <tazotodua@gmail.com>
2015-05-08 17:00:19 +0400

    Update export-mysql-database-sql-backup.php

--- export-mysql-database-sql-backup.php
+++ export-mysql-database-sql-backup.php
@@ -1,4 +1,4 @@
-======================USAGE=====================//to IMPORT, see the import.php file
+======================USAGE=====================//see the IMPORT.php too
 EXPORT_TABLES("localhost","user","pass","db_name" );  //or add 5th parameter(array of specific tables): array("mytable1","mytable2","mytable3")
 
 <?php

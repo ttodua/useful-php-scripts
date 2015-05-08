cd5c6491fb676e42ddaee9077d2e4d52b4340d66
Tazo Todua <tazotodua@gmail.com>
2015-05-08 17:02:24 +0400

    Update import-sql-database-mysql.php

--- import-sql-database-mysql.php
+++ import-sql-database-mysql.php
@@ -2,6 +2,7 @@
 IMPORT_TABLES("localhost","user","pass","db_name", "import.sql"); //TABLES ARE OVERWRITTEN
 
 <?php
+//https://github.com/tazotodua/useful-php-scripts
 function IMPORT_TABLES($host,$user,$pass,$dbname,$sql_file){
 	if (!file_exists($sql_file)) {die('Input the SQL filename correctly! <button onclick="window.history.back();">Click Back</button>');} $allLines = file($sql_file);
 	$mysqli = new mysqli($host, $user, $pass, $dbname); if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect_error();} 

95fc8e733913698027f33ad597766f49eddf493b
Tazo Todua <tazotodua@gmail.com>
2016-01-17 18:59:36 +0400

    Update export-mysql-database-sql-backup.php

--- export-mysql-database-sql-backup.php
+++ export-mysql-database-sql-backup.php
@@ -6,9 +6,11 @@ EXPORT_TABLES("localhost","user","pass","db_name" );  //or add 5th parameter(arr
 function EXPORT_TABLES($host,$user,$pass,$name,  $tables=false, $backup_name=false ){
 	$mysqli = new mysqli($host,$user,$pass,$name); $mysqli->select_db($name); $mysqli->query("SET NAMES 'utf8'");
 	$queryTables = $mysqli->query('SHOW TABLES'); while($row = $queryTables->fetch_row()) { $target_tables[] = $row[0]; }	if($tables !== false) { $target_tables = array_intersect( $target_tables, $tables); }
+	$content = "SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";\r\nSET time_zone = \"+00:00\";\r\n\r\n\r\n/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;\r\n/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;\r\n/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;\r\n/*!40101 SET NAMES utf8 */;\r\n--Database: `".$name."`\r\n\r\n\r\n";
+
 	foreach($target_tables as $table){
 		$result	= $mysqli->query('SELECT * FROM '.$table); 	$fields_amount=$result->field_count;  $rows_num=$mysqli->affected_rows; 	$res = $mysqli->query('SHOW CREATE TABLE '.$table);	$TableMLine=$res->fetch_row();
-		$content = (!isset($content) ?  '' : $content) . "\n\n".$TableMLine[1].";\n\n";
+		$content .= "\n\n".$TableMLine[1].";\n\n";
 		for ($i = 0, $st_counter = 0; $i < $fields_amount;   $i++, $st_counter=0) {
 			while($row = $result->fetch_row())	{ //when started (and every after 100 command cycle):
 				if ($st_counter%100 == 0 || $st_counter == 0 )	{$content .= "\nINSERT INTO ".$table." VALUES";}
@@ -20,6 +22,7 @@ function EXPORT_TABLES($host,$user,$pass,$name,  $tables=false, $backup_name=fal
 			}
 		} $content .="\n\n\n";
 	}
+	$content .= "\r\n\r\n/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;\r\n/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;\r\n/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;";
 	$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
 	header('Content-Type: application/octet-stream');	header("Content-Transfer-Encoding: Binary"); header("Content-disposition: attachment; filename=\"".$backup_name."\"");  echo $content; exit;
 }

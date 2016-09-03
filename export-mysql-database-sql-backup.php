9a484ba903d919a635faf499cf2d720bfb06f1e9
Tazo Todua <tazotodua@gmail.com>
2016-09-03 14:27:59 +0400

    Update export-mysql-database-sql-backup.php

--- export-mysql-database-sql-backup.php
+++ export-mysql-database-sql-backup.php
@@ -1,9 +1,11 @@
-<?php	// https://github.com/tazotodua/useful-php-scripts 
+<?php
+	// https://github.com/tazotodua/useful-php-scripts 
 	// EXAMPLE:     EXPORT_TABLES("localhost","user","pass","db_name" );  
-					//optional: 5th parameter(array) for specific tables: array("mytable1","mytable2","mytable3")   
-					//optional: 6th parameter(array) to adequatelly replace strings in DB!! MUST READ!!!! - https://goo.gl/2fZDQL
+					//optional: 5th parameter(array) - to backup specific tables only: array("mytable1","mytable2","mytable3")   
+    					//optional: 6th parameter(array) - backup filename
+    					// NOTE! to adequatelly replace strings in DB, MUST READ:  goo.gl/2fZDQL
 					
-function EXPORT_TABLES($host,$user,$pass,$name,       $tables=false, $backup_name=false, $replacements=array('OLD_DOMAIN.com','NEW_DOMAIN.com') ){ 
+function EXPORT_TABLES($host,$user,$pass,$name,       $tables=false, $backup_name=false){ 
 	set_time_limit(3000); $mysqli = new mysqli($host,$user,$pass,$name); $mysqli->select_db($name); $mysqli->query("SET NAMES 'utf8'");
 	$queryTables = $mysqli->query('SHOW TABLES'); while($row = $queryTables->fetch_row()) { $target_tables[] = $row[0]; }	if($tables !== false) { $target_tables = array_intersect( $target_tables, $tables); } 
 	$content = "SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";\r\nSET time_zone = \"+00:00\";\r\n\r\n\r\n/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;\r\n/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;\r\n/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;\r\n/*!40101 SET NAMES utf8 */;\r\n--\r\n-- Database: `".$name."`\r\n--\r\n\r\n\r\n";
@@ -20,7 +22,7 @@ function EXPORT_TABLES($host,$user,$pass,$name,       $tables=false, $backup_nam
 			}
 		} $content .="\n\n\n";
 	}
-	$content .= "\r\n\r\n/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;\r\n/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;\r\n/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;";                              if (function_exists('DOMAIN_or_STRING_modifier_in_DB')) { $content = DOMAIN_or_STRING_modifier_in_DB($replacements[0], $replacements[1], $content); }
+	$content .= "\r\n\r\n/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;\r\n/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;\r\n/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;";
 	$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
 	ob_get_clean(); header('Content-Type: application/octet-stream');	header("Content-Transfer-Encoding: Binary"); header("Content-disposition: attachment; filename=\"".$backup_name."\"");
 	echo $content; exit;

73574ede88a831530abaf393da6b2c39818c225e
Tazo Todua <tazotodua@gmail.com>
2016-11-17 13:13:27 +0400

    Update import-sql-database-mysql.php

--- import-sql-database-mysql.php
+++ import-sql-database-mysql.php
@@ -4,7 +4,7 @@
 
 // https://github.com/tazotodua/useful-php-scripts 
 function IMPORT_TABLES($host,$user,$pass,$dbname, $sql_file_OR_content){
-	set_time_limit(3000);  if(
+	set_time_limit(3000);
 	$SQL_CONTENT = (strlen($sql_file_OR_content) > 300 ?  $sql_file_OR_content : file_get_contents($sql_file_OR_content)  );  
 	$allLines = explode("\n",$SQL_CONTENT); 
 	$mysqli = new mysqli($host, $user, $pass, $dbname); if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect_error();} 
@@ -13,7 +13,7 @@ function IMPORT_TABLES($host,$user,$pass,$dbname, $sql_file_OR_content){
 	foreach ($allLines as $line)	{											// Loop through each line
 		if (substr($line, 0, 2) != '--' && $line != '') {$templine .= $line; 	// (if it is not a comment..) Add this line to the current segment
 			if (substr(trim($line), -1, 1) == ';') {		// If it has a semicolon at the end, it's the end of the query
-				$mysqli->query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . $mysqli->error . '<br /><br />');  $templine = ''; // set variable to empty, to start picking up the lines after ";"
+				if(!$mysqli->query($templine)){ print('Error performing query \'<strong>' . $templine . '\': ' . $mysqli->error . '<br /><br />');  }  $templine = ''; // set variable to empty, to start picking up the lines after ";"
 			}
 		}
 	}	return 'Importing finished. Now, Delete the import file.';

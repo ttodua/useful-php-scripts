2dcf9e24f76d423e7f407ed7f6f455ec544d89dc
Tazo Todua <tazotodua@gmail.com>
2016-11-17 12:45:54 +0400

    Update import-sql-database-mysql.php

--- import-sql-database-mysql.php
+++ import-sql-database-mysql.php
@@ -1,10 +1,10 @@
 <?ph	// https://github.com/tazotodua/useful-php-scripts  
 	// EXAMPLE:	IMPORT_TABLES("localhost","user","pass","db_name", "my_baseeee.sql"); //TABLES WILL BE OVERWRITTEN
-				//optional: 6th parameter-> array('OLD_DOMAIN.com','NEW_DOMAIN.com') to adequatelly replace strings in DB!! ( MUST READ!!!! - https://goo.gl/2fZDQL ) 
+				// If you need to replace STRINGS in DB (i.e. DOMAIN names replacements and etc),  at first ( instead of simple "STR_REPLACE") use this function :  MUST READ!!!! - https://goo.gl/2fZDQL ) 
 
 function IMPORT_TABLES($host,$user,$pass,$dbname, $sql_file_OR_content,      $replacements=array('','') ){
 	set_time_limit(3000);  if(
-	$SQL_CONTENT = (strlen($sql_file_OR_content) > 300 ?  $sql_file_OR_content : file_get_contents($sql_file_OR_content)  );        if (function_exists('DOMAIN_or_STRING_modifier_in_DB')) { $SQL_CONTENT = DOMAIN_or_STRING_modifier_in_DB($replacements[0], $replacements[1], $SQL_CONTENT); }
+	$SQL_CONTENT = (strlen($sql_file_OR_content) > 300 ?  $sql_file_OR_content : file_get_contents($sql_file_OR_content)  );  
 	$allLines = explode("\n",$SQL_CONTENT); 
 	$mysqli = new mysqli($host, $user, $pass, $dbname); if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect_error();} 
 		$zzzzzz = $mysqli->query('SET foreign_key_checks = 0');	        preg_match_all("/\nCREATE TABLE(.*?)\`(.*?)\`/si", "\n". $SQL_CONTENT, $target_tables); foreach ($target_tables[2] as $table){$mysqli->query('DROP TABLE IF EXISTS '.$table);}         $zzzzzz = $mysqli->query('SET foreign_key_checks = 1');    $mysqli->query("SET NAMES 'utf8'");	

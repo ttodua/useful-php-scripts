deef0bcd6b860750d5c0b6ae13a5319e54d495f6
Tazo Todua <tazotodua@gmail.com>
2017-12-07 21:11:54 +0400

    Update get-remote-url-content-data.php

--- get-remote-url-content-data.php
+++ get-remote-url-content-data.php
@@ -18,27 +18,19 @@ Notes:
 ############################################################################################
 */
 
-function get_remote_data($url, $post_paramtrs=false,            $extra_params=array('schemeless'=>'false','replace_src'=>false))	{
-	// set initial options
-	$GLOBALS['rdgr']['schemeless']	= array_key_exists('schemeless',	$extra_params)	? $extra_params['schemeless']	: false;
-	$GLOBALS['rdgr']['return_array']= array_key_exists('return_array',	$extra_params)	? $extra_params['return_array']	: false;
-	$GLOBALS['rdgr']['replace_src']	= array_key_exists('replace_src',	$extra_params)	? $extra_params['replace_src']	: false;
+function get_remote_data($url, $post_paramtrs=false,            $extra_params=array('schemeless'=>false,'replace_src'=>false))	{ 
 	// start curl
 	$c = curl_init();curl_setopt($c, CURLOPT_URL, $url);
 	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
 	//if parameters were passed to this function, then transform into POST method.. (if you need GET request, then simply change the passed URL)
 	if($post_paramtrs){ curl_setopt($c, CURLOPT_POST,TRUE);  curl_setopt($c, CURLOPT_POSTFIELDS, (is_array($post_paramtrs)? http_build_query($post_paramtrs) : $post_paramtrs) ); }
-	//check if JSON
-	if (is_object(json_decode($post_paramtrs))){
-		curl_setopt($c, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: '.strlen($post_paramtrs)) );
-	}
 	curl_setopt($c, CURLOPT_SSL_VERIFYHOST,false); 
 	curl_setopt($c, CURLOPT_SSL_VERIFYPEER,false);
-	curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0"); 
-	curl_setopt($c, CURLOPT_COOKIE, 'CookieName1=Value;');
-					//We'd better to use the above command, because the following command gave some weird STATUS results..
-					//$header[0]= $user_agent="User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0";  $header[]="Cookie:CookieName1=Value;"; $header[]="Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";  $header[]="Cache-Control: max-age=0"; $header[]="Connection: keep-alive"; $header[]="Keep-Alive: 300"; $header[]="Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7"; $header[] = "Accept-Language: en-us,en;q=0.5"; $header[] = "Pragma: ";  curl_setopt($c, CURLOPT_HEADER, true);     curl_setopt($c, CURLOPT_HTTPHEADER, $header);
-					
+	curl_setopt($c, CURLOPT_COOKIE, 'CookieName1=Value;'); 
+		$header[]= "User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0";	 $header[]= "Pragma: ";  $header[]= "Cache-Control: max-age=0";
+		if (is_object(json_decode($post_paramtrs))){ $header[]= 'Content-Type: application/json'; $header[]= 'Content-Length: '.strlen($post_paramtrs); }
+	curl_setopt($c, CURLOPT_HEADER, true); 
+	curl_setopt($c, CURLOPT_HTTPHEADER, $header);
 	curl_setopt($c, CURLOPT_MAXREDIRS, 10); 
 	//if SAFE_MODE or OPEN_BASEDIR is set,then FollowLocation cant be used.. so...
 	$follow_allowed= ( ini_get('open_basedir') || ini_get('safe_mode')) ? false:true;  if ($follow_allowed){curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);}
@@ -48,71 +40,8 @@ function get_remote_data($url, $post_paramtrs=false,            $extra_params=ar
 	curl_setopt($c, CURLOPT_AUTOREFERER, true);  
 	curl_setopt($c, CURLOPT_ENCODING, 'gzip,deflate');
 	$data=curl_exec($c); $status=curl_getinfo($c); curl_close($c);
-
-	$GLOBALS['rdgr']['parsed_url']	= parse_url($status['url']);
-	$GLOBALS['rdgr']['parsed_url']['base_link']	= $status['url'];
-	$GLOBALS['rdgr']['parsed_url']['domain_X']	= $GLOBALS['rdgr']['parsed_url']['scheme'].'://'.$GLOBALS['rdgr']['parsed_url']['host'];
-	$GLOBALS['rdgr']['parsed_url']['path_X']	= stripslashes(dirname($GLOBALS['rdgr']['parsed_url']['path']).'/'); 
-
-	$GLOBALS['rdgr']['ext_array'] 	= array(
-		'src'	=> array('audio','embed','iframe','img','input','script','source','track','video'),
-		'srcset'=> array('source'),
-		'data'	=> array('object'),
-		'href'	=> array('a','link','area'),
-		'action'=> array('form')
-		//'param', 'applet' and 'base' tags are exclusion, because of a bit complex structure 
-	);
-	$GLOBALS['rdgr']['all_protocols']= array('adc','afp','amqp','bacnet','bittorrent','bootp','camel','dict','dns','dsnp','dhcp','ed2k','empp','finger','ftp','gnutella','gopher','http','https','imap','irc','isup','javascript','ldap','mime','msnp','map','modbus','mosh','mqtt','nntp','ntp','ntcip','openadr','pop3','radius','rdp','rlogin','rsync','rtp','rtsp','ssh','sisnapi','sip','smtp','snmp','soap','smb','ssdp','stun','tup','telnet','tcap','tftp','upnp','webdav','xmpp');
-
-	$data= preg_replace_callback( 
-		'/<(((?!<).)*?)>/si', 	//avoids unclosed & closing tags
-		function($matches_A){
-			$returned = $matches_A[0];
-			$tagname = preg_match('/((.*?)(\s|$))/si', $matches_A[1], $n) ? $n[2] : "";
-			foreach($GLOBALS['rdgr']['ext_array'] as $key=>$value){
-				if(in_array($tagname,$value)){
-					preg_match('/ '.$key.'=(\'|\")/i', $returned, $n);
-					if(!empty($n[1])){
-						$GLOBALS['rdgr']['aphostrope_type']= $n[1];
-						$returned = preg_replace_callback( 
-							'/( '.$key.'='.$GLOBALS['rdgr']['aphostrope_type'].')(.*?)('.$GLOBALS['rdgr']['aphostrope_type'].')/i',
-							function($matches_B){
-								$full_link = $matches_B[2];
-								//correction to files/urls
-								if($GLOBALS['rdgr']['replace_src']	){
-									//if not schemeless url
-									if(substr($full_link, 0,2) != '//'){
-										$replace_allowed=true;
-										//check if the link is a type of any special protocol
-										foreach($GLOBALS['rdgr']['all_protocols'] as $each_protocol){
-											//if protocol found - dont continue
-											if(substr($full_link, 0, strlen($each_protocol)+1) == $each_protocol.':'){
-												$replace_allowed=false; break;
-											}
-										}
-										if($replace_allowed){
-											$full_link = $GLOBALS['rdgr']['parsed_url']['domain_X']. (str_replace('//','/',  $GLOBALS['rdgr']['parsed_url']['path_X'].$full_link) );
-										}
-									}
-								}
-								//replace http(s) with sheme-less urls
-								if($GLOBALS['rdgr']['schemeless']){
-									$matches_B[2]=str_replace(  array('https://','http://'), '//', $full_link);
-								}
-								unset($matches_B[0]);
-								$returned=''; foreach ($matches_B as $each){$returned .= $each; }
-								return $returned;
-							},
-							$returned
-						);
-					}
-				}
-			}
-			return $returned;
-		},
-		$data
-	); 
-
+	//URLS correction
+	if(function_exists('url_corrections_for_content_HELPER')){	    $data= url_corrections_for_content_HELPER($data, $status['url'],   array('schemeless'=>!empty($extra_params['schemeless']), 'replace_src'=>!empty($extra_params['replace_src']), 'rawgit_replace'=>!empty($extra_params['rawgit_replace']) )  );    	}
 	// if redirected, then get that redirected page
 	if($status['http_code']==301 || $status['http_code']==302) { 
 		//if we FOLLOWLOCATION was not allowed, then re-get REDIRECTED URL
@@ -130,6 +59,82 @@ function get_remote_data($url, $post_paramtrs=false,            $extra_params=ar
 	}
 	// if not redirected,and nor "status 200" page, then error..
 	elseif ( $status['http_code'] != 200 ) { $data =  "ERRORCODE22 with $url<br/><br/>Last status codes:".json_encode($status)."<br/><br/>Last data got:$data";}
-	return ( $GLOBALS['rdgr']['return_array'] ? array('data'=>$data,'info'=>$status) : $data);
+	$answer = ( !empty($extra_params['return_array']) ? array('data'=>$data,'info'=>$status) : $data);
+	return $answer;    
+		}	   
+		function url_corrections_for_content_HELPER( $content=false, $url=false, 	$extra_opts=array('schemeless'=>false, 'replace_src'=>false, 'rawgit_replace'=>false) ) { 
+	$GLOBALS['rdgr']['schemeless'] =$extra_opts['schemeless'];
+	$GLOBALS['rdgr']['replace_src']=$extra_opts['replace_src'];
+	$GLOBALS['rdgr']['rawgit_replace']=$extra_opts['rawgit_replace'];
+	if($GLOBALS['rdgr']['schemeless'] || $GLOBALS['rdgr']['replace_src'] ) {
+		if($url) {
+			$GLOBALS['rdgr']['parsed_url']			= parse_url($url);
+			$GLOBALS['rdgr']['urlparts']['domain_X']= $GLOBALS['rdgr']['parsed_url']['scheme'].'://'.$GLOBALS['rdgr']['parsed_url']['host'];
+			$GLOBALS['rdgr']['urlparts']['path_X']	= stripslashes(dirname($GLOBALS['rdgr']['parsed_url']['path']).'/'); 
+			$GLOBALS['rdgr']['all_protocols']= array('adc','afp','amqp','bacnet','bittorrent','bootp','camel','dict','dns','dsnp','dhcp','ed2k','empp','finger','ftp','gnutella','gopher','http','https','imap','irc','isup','javascript','ldap','mime','msnp','map','modbus','mosh','mqtt','nntp','ntp','ntcip','openadr','pop3','radius','rdp','rlogin','rsync','rtp','rtsp','ssh','sisnapi','sip','smtp','snmp','soap','smb','ssdp','stun','tup','telnet','tcap','tftp','upnp','webdav','xmpp');
+		}
+		$GLOBALS['rdgr']['ext_array'] 	= array(
+			'src'	=> array('audio','embed','iframe','img','input','script','source','track','video'),
+			'srcset'=> array('source'),
+			'data'	=> array('object'),
+			'href'	=> array('link','area'),  //'a' is excluded, because that is set manually in source
+			'action'=> array('form')
+			//'param', 'applet' and 'base' tags are exclusion, because of a bit complex structure 
+		);
+		$content= preg_replace_callback( 
+			'/<(((?!<).)*?)>/si', 	//avoids unclosed & closing tags
+			function($matches_A){
+				$content_A = $matches_A[0];
+				$tagname = preg_match('/((.*?)(\s|$))/si', $matches_A[1], $n) ? $n[2] : "";
+				foreach($GLOBALS['rdgr']['ext_array'] as $key=>$value){
+					if(in_array($tagname,$value)){
+						preg_match('/ '.$key.'=(\'|\")/i', $content_A, $n);
+						if(!empty($n[1])){
+							$GLOBALS['rdgr']['aphostrope_type']= $n[1];
+							$content_A = preg_replace_callback( 
+								'/( '.$key.'='.$GLOBALS['rdgr']['aphostrope_type'].')(.*?)('.$GLOBALS['rdgr']['aphostrope_type'].')/i',
+								function($matches_B){
+									$full_link = $matches_B[2];
+									//correction to files/urls
+									if(!empty($GLOBALS['rdgr']['replace_src'])	){
+										//if not schemeless url
+										if(substr($full_link, 0,2) != '//'){
+											$replace_src_allow=true;
+											//check if the link is a type of any special protocol
+											foreach($GLOBALS['rdgr']['all_protocols'] as $each_protocol){
+												//if protocol found - dont continue
+												if(substr($full_link, 0, strlen($each_protocol)+1) == $each_protocol.':'){
+													$replace_src_allow=false; break;
+												}
+											}
+											if($replace_src_allow){
+												$full_link = $GLOBALS['rdgr']['urlparts']['domain_X']. (str_replace('//','/',  $GLOBALS['rdgr']['urlparts']['path_X'].$full_link) );
+											}
+										}
+									}
+									//replace http(s) with sheme-less urls
+									if(!empty($GLOBALS['rdgr']['schemeless'])){
+										$full_link=str_replace(  array('https://','http://'), '//', $full_link);
+									}
+									//replace github mime
+									if(!empty($GLOBALS['rdgr']['rawgit_replace'])){
+										$full_link= str_replace('//raw.githubusercontent.com/','//rawgit.com/', $full_link);
+									}
+									$matches_B[2]=$full_link;
+									unset($matches_B[0]);
+									$content_B=''; foreach ($matches_B as $each){$content_B .= $each; }
+									return $content_B;
+								},
+								$content_A
+							);
+						}
+					}
+				}
+				return $content_A;
+			},
+			$content
+		); 
+	}
+	return $content;
 }
 ?>

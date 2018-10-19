c985db843b98a661935cc569369749fe82c420ce
Tazo Todua <tazotodua@gmail.com>
2018-10-19 14:54:43 -0400

    Update filter-php-variable-sanitize.php

--- filter-php-variable-sanitize.php
+++ filter-php-variable-sanitize.php
@@ -73,6 +73,6 @@ echo '<tr><td><code>'.htmlentities(defined('ABSPATH') ? sanitize_html_class($tex
 echo '<tr><td><code>'.htmlentities(defined('ABSPATH') ? sanitize_file_name($text) : 'a-1-.@^').'</code></td><td><code>sanitize_file_name($text)</code></td></tr>';
 echo '<tr><td><code>'.htmlentities(defined('ABSPATH') ? sanitize_email($text) : '').'</code></td><td><code>sanitize_email($text)</code></td></tr>';
 echo $divideeeeeeeeer;
-echo '<tr><td cospan="2">source code at : <b>github.com/tazotodua</b><td></tr>';
+echo '<tr><td cospan="2">source code at : <b>github.com/tazotodua/useful-php-scripts</b><td></tr>';
 echo '</tbody></table>';
 ?>

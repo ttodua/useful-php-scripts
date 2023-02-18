d9156821ae119a2c45ed420f6680de3b6f8b2511
T. Todua <7117978+ttodua@users.noreply.github.com>
2023-02-18 22:11:13 +0400

    Update zip-folder.php

--- zip-folder.php
+++ zip-folder.php
@@ -1,40 +1,28 @@
-// =========== https://github.com/ttodua/useful-php-scripts ========== 
-//                                 USAGE:
-//     new GoodZipArchive('path/to/input/folder',    'path/to/output_zip_file.zip') ;
-// ======================================================================
+// =========== https://github.com/ttodua/useful-php-scripts ================ 
+// =========================================================================
+//     zip_folder('path/to/input/folder',   'path/to/output_zip_file.zip') ;
+// =========================================================================
 
-
-class GoodZipArchive extends ZipArchive 
-{
-	//@author Nicolas Heimann
-	public function __construct($a=false, $b=false) { $this->create_func($a, $b);  }
-	
-	public function create_func($input_folder=false, $output_zip_file=false)
+function zip_folder ($input_folder, $output_zip_file) {
+	$zipClass = new ZipArchive();
+	if($input_folder !== false && $output_zip_file !== false)
 	{
-		if($input_folder !== false && $output_zip_file !== false)
-		{
-			$res = $this->open($output_zip_file, ZipArchive::CREATE);
-			if($res === TRUE) 	{ $this->addDir($input_folder, basename($input_folder)); $this->close(); }
-			else  				{ echo 'Could not create a zip archive. Contact Admin.'; }
+		$res = $zipClass->open($output_zip_file, \ZipArchive::CREATE);
+		if($res === TRUE)   {
+			// Add a Dir with Files and Subdirs to the archive
+			$foldername = basename($input_folder);
+			$zipClass->addEmptyDir($foldername);
+			$foldername .= '/';         $input_folder .= '/';
+			// Read all Files in Dir
+			$dir = opendir ($input_folder);
+			while ($file = readdir($dir))    {
+				if ($file == '.' || $file == '..') continue;
+				// Rekursiv, If dir: GoodZipArchive::addDir(), else ::File();
+				$do = (filetype( $input_folder . $file) == 'dir') ? 'addDir' : 'addFile';
+				$zipClass->$do($input_folder . $file, $foldername . $file);
+			}
+			$zipClass->close(); 
 		}
+		else   { exit ('Could not create a zip archive, migth be write permissions or other reason. Contact admin.'); }
 	}
-	
-    // Add a Dir with Files and Subdirs to the archive
-    public function addDir($location, $name) {
-        $this->addEmptyDir($name);
-        $this->addDirDo($location, $name);
-    }
-
-    // Add Files & Dirs to archive 
-    private function addDirDo($location, $name) {
-        $name .= '/';         $location .= '/';
-      // Read all Files in Dir
-        $dir = opendir ($location);
-        while ($file = readdir($dir))    {
-            if ($file == '.' || $file == '..') continue;
-          // Rekursiv, If dir: GoodZipArchive::addDir(), else ::File();
-            $do = (filetype( $location . $file) == 'dir') ? 'addDir' : 'addFile';
-            $this->$do($location . $file, $name . $file);
-        }
-    } 
-}
+} 
